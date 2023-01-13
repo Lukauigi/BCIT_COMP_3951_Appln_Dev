7ccf0bcaa3a122333bdb7eff32b59443f87020f6
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-01-13 12:16:29 -0800

    Add lab 7 meterial

---
+++ Labs/InvestmentCalculator/InvestmentCalculator/InvestmentCalculator.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InvestmentCalculator.aspx.cs" Inherits="InvestmentCalculator.InvestmentCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheetInvestmentCalculator.css" rel="stylesheet" runat="server" />
+    <style type="text/css">
+        .auto-style1 {
+            height: 101px;
+            width: 102px;
+        }
+        .auto-style2 {
+            width: 50%;
+            border: 1px solid #66FFCC;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <img class="auto-style1" src="Images/goat_logo.png" />
+    <h1> Investment Calculator</h1>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="monthlyInvestmentLabel" runat="server" Text="Monthly Investment"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="monthlyInvestmentOptionDropdown" runat="server">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="AnnualInvestmentLabel" runat="server" Text="Annual Investment Rate"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="investmentRateTextBox" runat="server">2.5</asp:TextBox>
+                        <asp:RangeValidator ID="investmentRateValidator" runat="server" ErrorMessage="Interest must range from 1 to 12" ControlToValidate="investmentRateTextBox" MaximumValue="12.0" MinimumValue="1.0" Type="Double"></asp:RangeValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="numOfYearsLabel" runat="server" Text="Number of Years"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="yearsTextBox" runat="server">20</asp:TextBox>
+                        <asp:RangeValidator ID="yearsValidator" runat="server" ErrorMessage="Number of years should be between 1 and 45" ControlToValidate="yearsTextBox" MaximumValue="45" MinimumValue="1" Type="Integer"></asp:RangeValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="futureValueLabel" runat="server" Text="Future Value"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="valueLabel" runat="server" Text="Label"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="calculateButton" runat="server" Text="Calculate" OnClick="ButtonCalculate" />
+                    </td>
+                    <td>
+                        <asp:Button ID="clearButton" runat="server" Text="Clear" OnClick="ButtonClear" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Labs/InvestmentCalculator/InvestmentCalculator/InvestmentCalculator.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Lab 7 - ASP.net webform of a investment calculator app.
+/// Author: Lukasz Bednarek
+/// Date: March 30, 2022
+/// </summary>
+namespace InvestmentCalculator
+{
+
+    /// <summary>
+    /// Definitions of constant values.
+    /// </summary>
+    static class Constants
+    {
+        //Default values to place upon clear button call
+        public const double DefaultAnnualInvestmentRate = 2.5;
+        public const int DefaultNumOfYears = 20;
+        public const string DefaultFutureValue = "...";
+
+        //modifiers to change input values for calculation
+        public const int MonthsInAYear = 12;
+        public const int RateFromPercentage = 100;
+    }
+
+    /// <summary>
+    /// A calculator interface which calculates an investment over a period of time with monthly contributions and investment rate.
+    /// </summary>
+    public partial class InvestmentCalculator : System.Web.UI.Page
+    {
+        decimal futureValue;
+
+        /// <summary>
+        /// Initializes instance on page initialization (only once).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            futureValue = 0;
+        }
+
+        /// <summary>
+        /// On page render, initializes control properties.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+            if (!IsPostBack)
+                for (int i = 50; i <= 500; i += 50)
+                    monthlyInvestmentOptionDropdown.Items.Add(i.ToString());
+        }
+
+        /// <summary>
+        /// Calculates future money value based on monthly investment, annual interest rate, and number of years.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonCalculate(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                //init local variables for reference
+                int annualInvestment = Int32.Parse(monthlyInvestmentOptionDropdown.SelectedValue) * Constants.MonthsInAYear;
+                decimal annualInvestmentRate = decimal.Parse(investmentRateTextBox.Text) / Constants.RateFromPercentage;
+                int years = Int32.Parse(yearsTextBox.Text);
+
+                for (int i = 0; i < years; ++i) //use this forumla every year
+                {
+                    futureValue = (futureValue + annualInvestment) * (1 + annualInvestmentRate);
+                }
+                //Credit: https://stackoverflow.com/questions/105770/net-string-format-to-add-commas-in-thousands-place-for-a-number, Seibar
+                valueLabel.Text = String.Format("${0:N}", futureValue);
+            }
+        }
+
+        /// <summary>
+        /// Resets the monthly investment, annual interest rate, and number of years to their default values.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonClear(object sender, EventArgs e)
+        {
+            investmentRateTextBox.Text = Constants.DefaultAnnualInvestmentRate.ToString();
+            yearsTextBox.Text = Constants.DefaultNumOfYears.ToString();
+            valueLabel.Text = Constants.DefaultFutureValue;
+        }
+    }
+}
