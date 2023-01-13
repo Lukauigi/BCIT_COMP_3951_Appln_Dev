7f4937f8c2437da31730d3e79516f571a35d9132
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-01-13 12:17:16 -0800

    Add lab 8 material

---
+++ Labs/A01206494Lab8/A01206494Lab8/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace A01206494Lab8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridCustomers = new System.Windows.Forms.DataGridView();
+            this.customerDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerDBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridCustomers
+            // 
+            this.dataGridCustomers.AutoGenerateColumns = false;
+            this.dataGridCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCustomers.DataSource = this.customerDBDataSetBindingSource;
+            this.dataGridCustomers.Location = new System.Drawing.Point(12, 12);
+            this.dataGridCustomers.Name = "dataGridCustomers";
+            this.dataGridCustomers.Size = new System.Drawing.Size(414, 188);
+            this.dataGridCustomers.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.dataGridCustomers);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerDBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridCustomers;
+        private System.Windows.Forms.BindingSource customerDBDataSetBindingSource;
+    }
+}
+
---
+++ Labs/A01206494Lab8/A01206494Lab8/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Lab 8 - Design and implement a local DB with ADO.NET on a .NET windows application.
+/// Author: Lukasz Bednarek
+/// Date: April 2, 2022
+/// </summary>
+namespace A01206494Lab8
+{
+    /// <summary>
+    /// Form to display local database data.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        /// <summary>
+        /// Initializes the MainForm component.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets datagrid properties and calls upon data from local database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            dataGridCustomers.AutoGenerateColumns = true;
+            dataGridCustomers.DataSource = customerDBDataSetBindingSource;
+            GetData("select * from Customers");
+        }
+
+        /// <summary>
+        /// Fills a table with data and command from the local database.
+        /// </summary>
+        /// <param name="command"></param>
+        private void GetData(string command)
+        {
+            try
+            {
+                String connection = " Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\CustomerDB.mdf;Integrated Security=True";
+
+                dataAdapter = new SqlDataAdapter(command, connection);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                DataTable table = new DataTable();
+                dataAdapter.Fill(table);
+                customerDBDataSetBindingSource.DataSource = table.DefaultView;
+
+                dataGridCustomers.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+            } 
+            catch (SqlException)
+            {
+                MessageBox.Show("Verify if the connectionString is correct");
+            }
+        }
+    }
+}
